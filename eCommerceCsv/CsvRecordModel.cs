914702dff018272d28f12279e97e72b05312dd0e
Michele Festa <mikeus9908@gmail.com>
2023-10-22 19:00:27 +0200

    first commit

---
+++ eCommerceCsv/CsvRecordModel.cs
@@ -0,0 +1,13 @@
+﻿namespace eCommerceCsv;
+/// <summary>
+/// This model is used for parsing the csv model
+/// </summary>
+public record CsvRecordModel
+{
+    public int Id {get; init; }
+    public string Name {get; init;} = string.Empty;
+    public int Quantity {get; init;}
+    public float UnitPrice {get; init;}
+    public float PercentageDiscount {get; init;}
+    public string Buyer {get; init;} = string.Empty;
+}
