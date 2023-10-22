715576ab8bcd8605547e248aa84bb0caac4ae13a
Michele Festa <mikeus9908@gmail.com>
2023-10-23 00:32:27 +0200

    removing testing code

--- eCommerceCsv/EcommerceRunner.cs
+++ eCommerceCsv/EcommerceRunner.cs
@@ -6,12 +6,12 @@ public class EcommerceRunner
     {
         Console.WriteLine(" Starting eCommerceConsoleApp ".PadLeft(50, '=').PadRight(75,'='));
         // reading filename as an argument
-        /*if(args.Length != 1){
+        if(args.Length != 1){
             LogError("Not enough args: You need to provide a filename.");
             return;
-        }*/
+        }
 
-        FileInfo fileInfo = new FileInfo("C:\\Users\\mikeu\\Documents\\progettiIncompleti\\eCommerceCsv\\eCommerceCsv\\eCommerceList.csv");
+        FileInfo fileInfo = new FileInfo(args[0]);
         if(fileInfo.Exists == false){
             LogError($"Invalid filename {fileInfo.FullName}, please enter a valid one");
             return;
