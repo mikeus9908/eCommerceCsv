44ba27ad70f15732df63d1873cbb413343886fe9
Michele Festa <mikeus9908@gmail.com>
2023-10-22 19:28:57 +0200

    open file

---
+++ eCommerceCsv/EcommerceRunner.cs
@@ -0,0 +1,27 @@
+﻿namespace eCommerceCsv;
+
+public class EcommerceRunner
+{
+    public static void Main (string[] args)
+    {
+        Console.WriteLine(" Starting eCommerceConsoleApp ".PadLeft(50, '=').PadRight(75,'='));
+        // reading filename as an argument
+        if(args.Length != 1){
+            LogError("Not enough args: You need to provide a filename.");
+            return;
+        }
+
+        FileInfo fileInfo = new FileInfo(args[0]);
+        if(fileInfo.Exists == false){
+            LogError($"Invalid filename {fileInfo.FullName}, please enter a valid one");
+            return;
+        }
+
+    }
+
+    private static void LogError(string message){
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+}
